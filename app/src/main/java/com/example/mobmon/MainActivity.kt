0050e859c18a9c4ddb8bed451d9ed987752fdb9b
Joel Amundberg <joel.amundberg@gmail.com>
2020-11-29 20:00:41 +0100

    Fix connection to server

--- app/src/main/java/com/example/mobmon/MainActivity.kt
+++ app/src/main/java/com/example/mobmon/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.mobmon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.android.volley.toolbox.Volley
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.Request
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    // TODO: STORE THESE IN APPLICATION SETTINGS
+    private val user = "MSIAfterburner"
+    private val pass = "17cc95b4017d496f82"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,16 +31,24 @@ class MainActivity : AppCompatActivity() {
         // Instantiate the RequestQueue.
         val dataTextView = findViewById<TextView>(R.id.dataTextView)
         val queue = Volley.newRequestQueue(this)
-        val url = "https://www.google.com"
+        val url = "http://192.168.0.199:82/mahm"  // TODO: RETRIEVE APPLICATION IP FROM SETTINGS
 
         // Request a string response from the provided URL.
-        val stringRequest = StringRequest(Request.Method.GET, url,
+        val stringRequest = object: StringRequest(Request.Method.GET, url,
                 { response ->
                     // Display the first 500 characters of the response string.
-
-                    dataTextView.text = "Response is: ${response.substring(0, 500)}"
+                    dataTextView.text = "Response is: ${response.substring(0, 1500)}"
                 },
-                { dataTextView.text = "That didn't work!" })
+                { error -> dataTextView.text = "That didn't work! - ${error.networkResponse.statusCode} - ${error.networkResponse.allHeaders}" })
+        {
+            override fun getHeaders() : MutableMap<String,String> {
+                val headers = HashMap<String, String>()
+                val credentials = String.format("%s:%s", user, pass) // TODO: FORCE THIS TO USE APPLICATION SETTINGS FOR RETRIEVING USER/PASS
+                val auth = Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
+                headers["Authorization"] = "Basic $auth"
+                return headers
+            }
+        }
 
         // Add the request to the RequestQueue.
         queue.add(stringRequest)
