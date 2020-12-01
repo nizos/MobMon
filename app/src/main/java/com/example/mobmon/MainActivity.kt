bfa02de51593160edc57c3867504d51f4b8c2d3f
Nizar <nizar.selander@gmail.com>
2020-12-01 14:27:57 +0100

    Added connection Fields and loop handler

--- app/src/main/java/com/example/mobmon/MainActivity.kt
+++ app/src/main/java/com/example/mobmon/MainActivity.kt
@@ -2,59 +2,74 @@ package com.example.mobmon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock.sleep
 import android.text.method.ScrollingMovementMethod
 import android.util.Base64
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.android.volley.toolbox.Volley
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.Request
-import com.android.volley.Response
+import com.android.volley.RequestQueue
 import com.example.mobmon.data.MSIParser
-import org.json.JSONObject
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserFactory
-import java.io.InputStream
-import java.util.function.BiConsumer
 
 class MainActivity : AppCompatActivity() {
 
-    // TODO: STORE THESE IN APPLICATION SETTINGS
-    private val user = "MSIAfterburner"
-    private val pass = "17cc95b4017d496f82"
+    private var ipAddress: String = "http://192.168.87.128:82/mahm"
+    private var username: String = "MSIAfterburner"
+    private var password: String = "17cc95b4017d496f82"
+    private var interval: Long = 1000
+    lateinit var mainHandler: Handler
+    lateinit var dataTextView: TextView
+    lateinit var queue: RequestQueue
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dataButton = findViewById<Button>(R.id.dataButton)
-        dataButton?.setOnClickListener {
-            getData()
+        val ipAddressTextField = findViewById<TextView>(R.id.IpAddressTextField)
+        val usernameTextField = findViewById<TextView>(R.id.UsernameTextField)
+        val passwordTextField = findViewById<TextView>(R.id.PasswordTextField)
+        val updateIntervalTextField = findViewById<TextView>(R.id.UpdateIntervalTextField)
+
+        dataTextView = findViewById(R.id.dataTextView)
+        queue = Volley.newRequestQueue(this)
+        ipAddress = ipAddressTextField.text.toString()
+        username = usernameTextField.text.toString()
+        password = passwordTextField.text.toString()
+        interval = updateIntervalTextField.text.toString().toLong()
+        mainHandler = Handler(Looper.getMainLooper())
+        dataTextView.movementMethod = ScrollingMovementMethod()
+
+
+        val connectButton = findViewById<Button>(R.id.ConnectButton)
+        connectButton?.setOnClickListener {
+            mainHandler.post(object : Runnable {
+                override fun run() {
+                    connect()
+                    mainHandler.postDelayed(this, 1000)
+                }
+            })
         }
     }
 
-    fun getData() {
-        // Instantiate the RequestQueue.
-        val dataTextView = findViewById<TextView>(R.id.dataTextView)
-        dataTextView.movementMethod = ScrollingMovementMethod()
-        val queue = Volley.newRequestQueue(this)
-        val url = "http://192.168.0.199:82/mahm"  // TODO: RETRIEVE APPLICATION IP FROM SETTINGS
+    private fun connect() {
 
-        // Request a string response from the provided URL.
-        val stringRequest = object: StringRequest(Request.Method.GET, url,
+        val stringRequest = object: StringRequest(Request.Method.GET, "http://192.168.87.128:82/mahm",
                 { response -> dataTextView.text = stringifyReturn(MSIParser.parseMSIData(response, "UTF-8"))}, // TODO: Make encoding more dynamic.
-                { error -> dataTextView.text = "That didn't work! - ${error.networkResponse.statusCode} -\n ${error.networkResponse.allHeaders} \n" })
+                { error -> dataTextView.text = "That didn't work! - ${error.toString()} -\n" })
         {
             override fun getHeaders() : MutableMap<String,String> {
                 val headers = HashMap<String, String>()
-                val credentials = String.format("%s:%s", user, pass) // TODO: FORCE THIS TO USE APPLICATION SETTINGS FOR RETRIEVING USER/PASS
+                val credentials = String.format("%s:%s", "MSIAfterburner", "17cc95b4017d496f82") // TODO: FORCE THIS TO USE APPLICATION SETTINGS FOR RETRIEVING USER/PASS
                 val auth = Base64.encodeToString(credentials.toByteArray(), Base64.NO_WRAP)
                 headers["Authorization"] = "Basic $auth"
                 return headers
             }
         }
-
         // Add the request to the RequestQueue.
         queue.add(stringRequest)
     }
