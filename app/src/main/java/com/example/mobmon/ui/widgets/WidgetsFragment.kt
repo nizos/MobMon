fbe095afcf275143f7fa4da97109c12d9889060f
Epicoustic <martin3127@gmail.com>
2020-12-10 23:38:03 +0100

    widget list functionality added

--- app/src/main/java/com/example/mobmon/ui/widgets/WidgetsFragment.kt
+++ app/src/main/java/com/example/mobmon/ui/widgets/WidgetsFragment.kt
@@ -1,18 +1,19 @@
 package com.example.mobmon.ui.widgets
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import com.example.mobmon.R
-import com.example.mobmon.controller.MainController
 import com.example.mobmon.controller.MainController.metricsData
+
 
 class WidgetsFragment : Fragment() {
 
@@ -20,21 +21,55 @@ class WidgetsFragment : Fragment() {
     var keyStringArray: MutableList<String> = mutableListOf<String>()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         widgetsViewModel =
                 ViewModelProvider(this).get(WidgetsViewModel::class.java)
 
         val root = inflater.inflate(R.layout.fragment_widgets, container, false)
+        var listViewOfWidgets = root.findViewById<ListView>(R.id.widget_list)
+        listViewOfWidgets.adapter = ArrayAdapter<String>(requireContext(), R.layout.listrow, keyStringArray)
+        //listViewOfWidgets.adapter = CustomAdapter(requireContext())
         //TODO: metricsData.value can be null
         metricsData.observe(viewLifecycleOwner, Observer {
             keyStringArray.clear()
             metricsData.value?.forEach { keyStringArray.add(it.key) }
-            Log.i("Widgets","${keyStringArray}")
+            Log.i("Widgets", "${keyStringArray}")
+            //listViewOfWidgets.adapter = adapter
         });
 
+        listViewOfWidgets.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(requireContext(),keyStringArray[position],Toast.LENGTH_SHORT).show()
+        }
+
         return root
+    }
+
+    private class CustomAdapter(context: Context): BaseAdapter() {
+        private val mContext: Context
+
+        init {
+            mContext = context
+        }
+
+        override fun getCount(): Int {
+            return 5
+        }
+
+        override fun getItem(position: Int): Any {
+            return position
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val textView = TextView(mContext)
+            textView.text = "Here is my row"
+            return textView
+        }
     }
 }
